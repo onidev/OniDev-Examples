40c435505d173287e8c20fdb94d1bffc52343694
Boris M <onilink_@hotmail.fr>
2015-04-09 13:51:56 +0200

    Create composite.cpp

---
+++ Various/Verlet/composite.cpp
@@ -0,0 +1,43 @@
+#include "composite.h"
+#include <algorithm>
+
+std::shared_ptr<Particle> Composite::addParticle(const od::Vec2 & pos)
+{
+    return addParticle(pos, od::Vec2(0, 0));
+}
+
+std::shared_ptr<Particle> Composite::addParticle(const od::Vec2 & pos, const od::Vec2 & vel)
+{
+    std::shared_ptr<Particle> pt(new Particle(pos, vel));
+    _particles.push_back(pt);
+    return pt;
+}
+
+bool Composite::removeParticle(std::shared_ptr<Particle> & pt)
+{
+    auto it = std::find(_particles.begin(), _particles.end(), pt);
+    if(it != _particles.end())
+    {
+        _particles.erase(it);
+        return true;
+    }
+    return false;
+}
+
+bool Composite::removeConstraint(std::shared_ptr<Constraint> & c)
+{
+    auto it = std::find(_constraints.begin(), _constraints.end(), c);
+    if(it != _constraints.end())
+    {
+        _constraints.erase(it);
+        return true;
+    }
+    return false;
+}
+
+std::shared_ptr<Particle> Composite::getParticleAt(unsigned int ind)
+{
+    if(ind < _particles.size())
+        return _particles[ind];
+    return nullptr;
+}
