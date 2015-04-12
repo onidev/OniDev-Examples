619650d8392107f67ddecf5ad4eca44629e7a567
Boris M <onilink_@hotmail.fr>
2015-04-12 11:59:35 +0200

    Create make_unique.h

---
+++ Various/Verlet3d/make_unique.h
@@ -0,0 +1,14 @@
+#ifndef MAKE_UNIQUE_H_INCLUDED
+#define MAKE_UNIQUE_H_INCLUDED
+
+namespace std
+{
+
+template<typename T, typename... Args>
+std::unique_ptr<T> make_unique(Args&&... args) {
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
+}
+
+#endif // MAKE_UNIQUE_H_INCLUDED
